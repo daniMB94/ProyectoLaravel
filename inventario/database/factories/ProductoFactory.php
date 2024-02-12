d7e7392c4b24292255093f676a6ecce704b13499
daniMB94 <dani_aliast@hotmail.com>
2024-02-12 21:09:16 +0100

    migraciones, modelos, seeders y llenado de datos

--- inventario/database/factories/ProductoFactory.php
+++ inventario/database/factories/ProductoFactory.php
@@ -17,7 +17,15 @@ class ProductoFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'codigo' => 'PRO_COD_'. $this->faker->text($maxNbChars = 5),
+            'modelo' => 'Modelo ' . $this->faker->text($maxNbChars = 10),
+            'fabricante' => $this->faker->name . ' & Company',
+            'descripcion' => $this->faker->text($maxNbChars = 100),
+            'imagen' => $this->faker->numberBetween(1, 10) . '.png',
+            'stock' => $this->faker->numberBetween(15, 600),
+            'estado' => $this->faker->randomElement(['activo', 'roto', 'desaparecido']),
+            'localizacion_id' => $this->faker->numberBetween(1, 15),
+            'categoria_id' => $this->faker->numberBetween(1, 6),
         ];
     }
 }
--- inventario/database/seeders/ProductoSeeder.php
+++ inventario/database/seeders/ProductoSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\models\Producto;
 
 class ProductoSeeder extends Seeder
 {
@@ -12,6 +13,6 @@ class ProductoSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Producto::factory()->count(100)->create();
     }
 }
